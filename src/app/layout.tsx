cb985603288878b76613ff3dbc23117550027d59
EG <37511853+Lefgk@users.noreply.github.com>
2025-11-22 11:17:58 +0700

    v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'PulseChain Marketplace',
-  description: 'Find goods and services from the PulseChain community',
+    title: 'PulseChain Marketplace',
+    description: 'Find goods and services from the PulseChain community',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
